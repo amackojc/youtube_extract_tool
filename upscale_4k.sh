b902491efdd27811e8a7ae0d2a48ab692a78cf7b
Aleksander Mackojć <aleksander.mackojc@gmail.com>
2021-12-12 01:49:55 +0100

    Refactor upscaling frames to 4k

--- upscale_4k.sh
+++ upscale_4k.sh
@@ -2,18 +2,32 @@
 
 
 WORKSPACE=$(pwd)
+FRAME_OUTPUT_RESOLUTION='3840:2160'
+INPUT_FOLDER='FRAMES'
+OUTPUT_FOLDER='UPSCALED_FRAMES'
+DISPLAY_ASPECT_RATIO='1:1'
+source "${WORKSPACE}/common.sh"
 
-if [[ -d "${WORKSPACE}/UPSCALED_FRAMES" ]]
-then
-    echo "Directory already exists"
-    rm -r "${WORKSPACE}/UPSCALED_FRAMES"/*
-else
-    mkdir -p "${WORKSPACE}/UPSCALED_FRAMES"
-fi
+function upscale_frame {
+    ffmpeg -i "${1}" \
+           -vf scale="${FRAME_OUTPUT_RESOLUTION}",setsar="${DISPLAY_ASPECT_RATIO}" "${2}"
+}
 
-for file in "${WORKSPACE}/FRAMES/"*.png; do
-    file_name=$(echo "${file}" | awk -F "/" ' { print$NF } ')
-    echo "${WORKSPACE}/UPSCALED_FRAMES/${file_name}"
-    ffmpeg -i "${file}" -vf scale=3840:2160,setsar=1:1 "${WORKSPACE}/UPSCALED_FRAMES/${file_name}"
-done
+function run {
+    prepare_directory "${WORKSPACE}/${OUTPUT_FOLDER}"
+    for directory in "${WORKSPACE}/${INPUT_FOLDER}/"*; do
+        if [ -d "${directory}" ];then
+            output_directory_name=$(get_file_from_fullpath "${directory}")
+            output_directory="${WORKSPACE}/${OUTPUT_FOLDER}/${output_directory_name}_UPSCALED"
+            prepare_directory "${output_directory}"
+            for frame in "${directory}"/*.png; do
+                output_file_name=$(get_file_from_fullpath "${frame}")
+                upscale_frame "${frame}" "${output_directory}/${output_file_name}"
+                #upscale_frame "${frame}" "${directory}/${output_file_name}"
+                #ffmpeg -i "${file}" -vf scale=3840:2160,setsar=1:1 "${WORKSPACE}/UPSCALED_FRAMES/${file_name}"
+            done
+        fi
+    done
+}
 
+run
