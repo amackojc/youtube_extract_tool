07c013f427d5a69231ea5e3b67ccafd131b3574e
Aleksander Mackojć <aleksander.mackojc@gmail.com>
2021-12-12 03:44:03 +0100

    Added cleanup function

--- common.sh
+++ common.sh
@@ -2,13 +2,17 @@
 
 # There I will be storing repeated func from other scripts
 
-function create_directory() {
+function create_directory {
     if [[ ! -d "${1}" ]]; then
         mkdir -p "${1}"
     fi
 }
 
-function get_file_from_fullpath () {
+function get_file_from_fullpath {
     echo "${1}" | \
     awk -F '/' '{ print$NF }'
+}
+
+function clean_up_content {
+    rm -r "${1}"
 }
