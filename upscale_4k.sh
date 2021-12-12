abe051b1f2d03f8800c3808e08af179b350fb8ea
Aleksander Mackojć <aleksander.mackojc@gmail.com>
2021-12-12 03:45:06 +0100

    upscale script refactor to be more reable

--- upscale_4k.sh
+++ upscale_4k.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
 
-
 WORKSPACE=$(pwd)
 FRAME_OUTPUT_RESOLUTION='3840:2160'
 INPUT_FOLDER='FRAMES'
-OUTPUT_FOLDER='UPSCALED_FRAMES'
+OUTPUT_FOLDER="${WORKSPACE}/UPSCALED_FRAMES"
+SET_SCALE_CONFIG="scale=${FRAME_OUTPUT_RESOLUTION}"
+SETSAR_CONFIG="setsar=${DISPLAY_ASPECT_RATIO}"
+
 DISPLAY_ASPECT_RATIO='1:1'
 source "${WORKSPACE}/common.sh"
 
 function upscale_frame {
     ffmpeg -i "${1}" \
-           -vf scale="${FRAME_OUTPUT_RESOLUTION}",setsar="${DISPLAY_ASPECT_RATIO}" "${2}"
+           -vf "${SET_SCALE_CONFIG}","${SETSAR_CONFIG}" "${2}"
 }
 
-function run {
-    prepare_directory "${WORKSPACE}/${OUTPUT_FOLDER}"
+function prepare_output_directory {
+    for directory in "${WORKSPACE}/${INPUT_FOLDER}/"*; do
+        if [ -d "${directory}" ]; then
+            directory_name=$(get_file_from_fullpath "${directory}")
+            output_directory="${OUTPUT_FOLDER}/${directory_name}_UPSCALED"
+            create_directory "${output_directory}"
+        fi
+    done
+
+}
+
+function frames_upscaling_process {
     for directory in "${WORKSPACE}/${INPUT_FOLDER}/"*; do
         if [ -d "${directory}" ];then
-            output_directory_name=$(get_file_from_fullpath "${directory}")
-            output_directory="${WORKSPACE}/${OUTPUT_FOLDER}/${output_directory_name}_UPSCALED"
-            prepare_directory "${output_directory}"
+            directory_name=$(get_file_from_fullpath "${directory}")
+            output_directory="${OUTPUT_FOLDER}/${directory_name}_UPSCALED"
             for frame in "${directory}"/*.png; do
                 output_file_name=$(get_file_from_fullpath "${frame}")
-                upscale_frame "${frame}" "${output_directory}/${output_file_name}"
-                #upscale_frame "${frame}" "${directory}/${output_file_name}"
-                #ffmpeg -i "${file}" -vf scale=3840:2160,setsar=1:1 "${WORKSPACE}/UPSCALED_FRAMES/${file_name}"
+                upscale_frame "${frame}" \
+                              "${output_directory}/${output_file_name}"
             done
         fi
     done
+}
+
+function run {
+    clean_up_content "${OUTPUT_FOLDER}"
+    create_directory "${OUTPUT_FOLDER}"
+    prepare_output_directory
+    frames_upscaling_process
 }
 
 run
