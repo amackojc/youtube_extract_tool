d70546f008cfc96cab89587e0730bc6ed825961c
Aleksander Mackojć <aleksander.mackojc@gmail.com>
2021-12-13 10:46:47 +0100

    Refactor splitting script

--- split_into_blocks.sh
+++ split_into_blocks.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
 
 WORKSPACE=$(pwd)
+
+INPUT_FOLDER='UPSCALED_FRAMES'
+OUTPUT_FOLDER="${WORKSPACE}/BLOCKS"
+
+HEIGHT_BLOCK=100
+WIDTH_BLOCK=100
+
 source "${WORKSPACE}/common.sh"
 
-prepare_directories "BLOCKS"
-
-HEIGHT_BLOCK=1000
-WIDTH_BLOCK=1000
-
-for image in "${WORKSPACE}/UPSCALED_FRAMES/"*png; do
-    input_file=$(
-        echo "${image}" | \
-        awk -F "/" '{ print$NF }'
-    ) 
-    output_file=$(
-        echo "${input_file}" | \
-        sed  -e 's/:/_/g' \
-             -e 's/ //g'
-    )
+function split_into_block {
     convert \
         -crop "${HEIGHT_BLOCK}"x"${WIDTH_BLOCK}" \
-        "${WORKSPACE}/UPSCALED_FRAMES/${input_file}" \
-        "${WORKSPACE}/BLOCKS/${output_file}"
-    count+=1
-done
+        "${1}" \
+        "${2}"
+}
 
+function splitting_process {
+    echo "Splliting..."
+    for directory in "${1}"/*; do
+        if [ -d "${directory}" ]; then
+            directory_name=$(get_file_from_fullpath "${directory}")
+            output_directory="${2}/${directory_name}_${3}"
+            if check_directory_is_empty "${directory}"; then
+                continue
+            fi
+            for image in "${directory}"/*.png; do
+                output_file=$(get_file_from_fullpath "${image}")
+                echo "${output_file}"
+                split_into_block "${image}" \
+                                 "${output_directory}/${output_file}"
+            done
+        fi
+    done
+    echo "Done!"
+}
 
+function run {
+    create_directory "${OUTPUT_FOLDER}"
+    clean_up_content "${OUTPUT_FOLDER}"
+    prepare_output_directory "${WORKSPACE}/${INPUT_FOLDER}" \
+                             "${OUTPUT_FOLDER}" \
+                             "BLOCKS"
+
+    splitting_process "${WORKSPACE}/${INPUT_FOLDER}" \
+                      "${OUTPUT_FOLDER}" \
+                      "BLOCKS"
+}
+
+run
