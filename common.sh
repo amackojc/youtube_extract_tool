3dabd031e222f15178d610cc00323fca39e348f4
Aleksander Mackojć <aleksander.mackojc@gmail.com>
2021-12-13 10:43:23 +0100

    Added fucntion for checking that directory is empty

--- common.sh
+++ common.sh
@@ -14,5 +14,31 @@ function get_file_from_fullpath {
 }
 
 function clean_up_content {
-    rm -r "${1}"
+    if ! rm -r "${1}"/* 2> /dev/null; then
+        echo "Nothing to clean..."
+    fi
+}
+
+function prepare_output_directory {
+    for directory in "${1}"/*; do
+        if [ -d "${directory}" ]; then
+            directory_name=$(get_file_from_fullpath "${directory}")
+            output_directory="${2}/${directory_name}_${3}"
+            create_directory "${output_directory}"
+        fi
+    done
+}
+
+function check_directory_is_empty {
+    number_of_files=$(
+        find "${1}" -type f | \
+        wc -l
+    )
+    dir=$(get_file_from_fullpath "${1}")
+    if [[ ${number_of_files} == 0 ]]; then
+        echo "${directory} is empty!"
+        return 0
+    else
+        return 1
+    fi
 }
