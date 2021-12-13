cb6898e5311b72aa4cb51cef1377bf30c0e3c1c5
Aleksander Mackojć <aleksander.mackojc@gmail.com>
2021-12-13 10:47:53 +0100

    More parametrized functions

--- upscale_4k.sh
+++ upscale_4k.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 WORKSPACE=$(pwd)
-FRAME_OUTPUT_RESOLUTION='3840:2160'
+
 INPUT_FOLDER='FRAMES'
 OUTPUT_FOLDER="${WORKSPACE}/UPSCALED_FRAMES"
+
+FRAME_OUTPUT_RESOLUTION='3840:2160'
 SET_SCALE_CONFIG="scale=${FRAME_OUTPUT_RESOLUTION}"
 SETSAR_CONFIG="setsar=${DISPLAY_ASPECT_RATIO}"
-
 DISPLAY_ASPECT_RATIO='1:1'
+
 source "${WORKSPACE}/common.sh"
 
 function upscale_frame {
@@ -15,22 +17,11 @@ function upscale_frame {
            -vf "${SET_SCALE_CONFIG}","${SETSAR_CONFIG}" "${2}"
 }
 
-function prepare_output_directory {
-    for directory in "${WORKSPACE}/${INPUT_FOLDER}/"*; do
-        if [ -d "${directory}" ]; then
-            directory_name=$(get_file_from_fullpath "${directory}")
-            output_directory="${OUTPUT_FOLDER}/${directory_name}_UPSCALED"
-            create_directory "${output_directory}"
-        fi
-    done
-
-}
-
 function frames_upscaling_process {
-    for directory in "${WORKSPACE}/${INPUT_FOLDER}/"*; do
+    for directory in "${1}"/*; do 
         if [ -d "${directory}" ];then
             directory_name=$(get_file_from_fullpath "${directory}")
-            output_directory="${OUTPUT_FOLDER}/${directory_name}_UPSCALED"
+            output_directory="${2}/${directory_name}_${3}"
             for frame in "${directory}"/*.png; do
                 output_file_name=$(get_file_from_fullpath "${frame}")
                 upscale_frame "${frame}" \
@@ -41,10 +32,15 @@ function frames_upscaling_process {
 }
 
 function run {
-    clean_up_content "${OUTPUT_FOLDER}"
     create_directory "${OUTPUT_FOLDER}"
-    prepare_output_directory
-    frames_upscaling_process
+    clean_up_content "${OUTPUT_FOLDER}"
+    prepare_output_directory "${WORKSPACE}/${INPUT_FOLDER}" \
+                             "${OUTPUT_FOLDER}" \
+                             "UPSCALED"
+
+    frames_upscaling_process "${WORKSPACE}/${INPUT_FOLDER}" \
+                             "${OUTPUT_FOLDER}" \
+                             "UPSCALED"
 }
 
 run
